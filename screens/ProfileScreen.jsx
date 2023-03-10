8ef62898a77775c317ec93602244e1192d35dc44
yhc125 <yhc125r@naver.com>
2023-03-10 23:26:04 +0900

    ProfileScreen 위치 데이터 삽입

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,29 +1,71 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React, { Component, useLayoutEffect } from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { WebView } from 'react-native-webview';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import LocationPermission from '../components/LocationPermission';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Location from 'expo-location';
 
-
-class MyWeb extends Component {
-	render() {
-		return (
-			<WebView
-				source={{ uri: 'https://www.youtube.com/' }}
-			/>
-		);
-	}
-}
 
 const ProfileScreen = () => {
 	const navigation = useNavigation();
+
+	const [savedData, setSavedData] = useState('');
+
+	const [location, setLocation] = useState(null);
+	const webViewRef = useRef(null);
+
+	const handleReload = () => {
+		if (webViewRef.current) {
+			webViewRef.current.reload();
+
+			const fetchLocation = async () => {
+				// 현재 위치 정보 가져오기
+				let { status } = await Location.requestForegroundPermissionsAsync();
+				if (status !== 'granted') {
+					console.log('Permission to access location was denied');
+					return;
+				}
+				let location = await Location.getCurrentPositionAsync({});
+				const currentLocation = {
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude
+				};
+				// myData 키로 위치 정보를 저장
+				const data = JSON.stringify({ currentLocation });
+				await AsyncStorage.setItem('myData', data);
+				setSavedData(data);
+				console.log('fetch', data);
+			};
+
+			fetchLocation();
+		}
+
+	};
+
+	const injectedJavaScript = `
+	window.onload = function() {
+		document.getElementById('current-location').innerHTML = 'Current Location: ${JSON.stringify(savedData)}';
+	}
+	
+`;
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await AsyncStorage.getItem('myData');
+			setSavedData(data);
+		};
+		fetchData();
+	}, []);
+
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerShown: false,
 		});
 	}, []);
-	
+
 	return (
 		<SafeAreaView className="flex-1 bg-white">
 			{/* {Header} */}
@@ -35,7 +77,16 @@ const ProfileScreen = () => {
 					</Text>
 				</View>
 			</View>
-			<MyWeb />
+			<WebView
+				ref={webViewRef}
+				source={require('../assets/webview/index.html')}
+				injectedJavaScript={injectedJavaScript}
+			/>
+			<View>
+				<Text>Saved data: {savedData}</Text>
+				<Button title="Reload" onPress={handleReload} />
+			</View>
+
 		</SafeAreaView>
 	)
 }
